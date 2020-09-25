0331bf815b7caef28b135291db2717d2af04dd72
Hugo Vieira Neto <hvieir@utfpr.edu.br>
2020-09-25 17:03:55 -0300

    Initial commit

---
+++ Debug/FreeRTOS/Source/portable/GCC/ARM_CM3/port.d
@@ -0,0 +1,32 @@
+FreeRTOS/Source/portable/GCC/ARM_CM3/port.o: \
+ ../FreeRTOS/Source/portable/GCC/ARM_CM3/port.c \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/FreeRTOS.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu/FreeRTOSConfig.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\Libraries\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/projdefs.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/portable.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/deprecated_definitions.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\portable\GCC\ARM_CM3/portmacro.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/mpu_wrappers.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/task.h \
+ C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/list.h
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/FreeRTOS.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu/FreeRTOSConfig.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\Libraries\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/projdefs.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/portable.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/deprecated_definitions.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\portable\GCC\ARM_CM3/portmacro.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/mpu_wrappers.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/task.h:
+
+C:\Users\hviei\eclipse-workspace\tasks_FreeRTOS_qemu\FreeRTOS\Source\include/list.h:
